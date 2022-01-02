4267eea0d4aff9c476bd3fe4f707f4e6f6ce806d
yasith wimukthi <it19966922@my.sliit.lk>
2022-01-02 21:44:52 +0530

    initial commit

---
+++ src/components/ui/header/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return(
+        <div className="header-container">
+            <div className="search-bar">
+
+            </div>
+            <div className="navigation-bar">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
