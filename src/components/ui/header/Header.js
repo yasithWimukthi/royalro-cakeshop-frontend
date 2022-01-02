553d9e14f793ed6376a24cabb563c07130871f5a
yasith wimukthi <it19966922@my.sliit.lk>
2022-01-02 22:38:31 +0530

    implement header

--- src/components/ui/header/Header.js
+++ src/components/ui/header/Header.js
@@ -1,8 +1,25 @@
+import React from "react";
+import './header.css';
+import {Button} from "antd";
+import {SearchOutlined} from "@ant-design/icons";
+
+
 const Header = () => {
     return(
         <div className="header-container">
             <div className="search-bar">
-
+                <div className="logo-box">
+                    <div className="logo-container"></div>
+                    <p>Royalro</p>
+                </div>
+                <div className="search-bar-container">
+                    <form>
+                        <input type="text" className="search-input" placeholder="Search"/>
+                        <Button className="search-btn" type="primary" icon={<SearchOutlined />}>
+                            <span className="search-text">Search</span>
+                        </Button>
+                    </form>
+                </div>
             </div>
             <div className="navigation-bar">
 
